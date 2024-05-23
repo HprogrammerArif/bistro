031df46e1a58fdf92d7651cb5363fd0f6d55bd72
HprogrammerArif <programmerhabluarif@gmail.com>
2024-05-23 14:56:33 +0600

    added signin signup and firebase, provide with dot env file and creadintial

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
+  const location = useLocation();
+  const noHeaderFooter = location.pathname.includes("login") || location.pathname.includes("signup");
   return (
     <div>
-      <Navbar></Navbar>
+      {noHeaderFooter || <Navbar></Navbar>}
+      <div className="min-h-[calc(100vh-240px)]">
       <Outlet></Outlet>
-      <Footer></Footer>
+      </div>
+      {noHeaderFooter || <Footer></Footer>}
     </div>
   );
 };
